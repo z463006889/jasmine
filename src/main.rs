02b97fdb84375f0613d419ce8c61ce396eff14f0
千本的饭后橙子 <463006889@qq.com>
2024-06-09 22:38:55 +0800

    modify

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
 
-fn main() {
-    println!("Hello, Rust!");
+fn add(x:i32,y:i32) ->i32{
+    x+y
 }
+
+enum Types<T> {
+    AniI32(T),
+    Nothing,
+}
+
+fn main() {
+    let mut v=vec![1,2,3];
+    v.push(4);
+    println!("Hello, Rust!");
+    println!("{:?}",v);
+    let t: Types<i32>=Types::AniI32(20);
+    match t {
+        Types::AniI32(f)=>println!("{}",f),
+        Types::Nothing=>println!("Nothing"),
+    };
+}
+
